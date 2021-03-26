37f19dd87bfaebe13bfeca84a94554c78321e840
Existance29 <54537576+Existance29@users.noreply.github.com>
2021-03-27 07:34:58 +0800

    Add files via upload

---
+++ RPG.py
@@ -0,0 +1,22 @@
+end = 1
+
+import json
+
+with open('spells.json') as f:
+  spellData= json.load(f)
+
+while(end):
+    command = input("Input a command: ")
+
+    if command.startswith('info'):
+
+        a = command.split()[1].lower()
+        for key in spellData:
+            print(key)
+            if a == key['name'].lower().replace(" ", ""):
+                print(key['desc'])
+                break
+            
+
+        
+
